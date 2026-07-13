8fa9360e358cd4429ae46ef9d717442d79fbf789
hoangphi117 <nghphi1206@gmail.com>
2026-07-13 12:07:42 +0700

    feat(data): remove unused methods for customer management in KhachHangDB

--- src/HomeStay.Application/DataAccess/DBs/KhachHangDB.cs
+++ src/HomeStay.Application/DataAccess/DBs/KhachHangDB.cs
@@ -35,20 +35,4 @@ public static class KhachHangDB
             throw new InvalidOperationException("Không thể cập nhật khách hàng.");
     }
 
-    public static async Task<KhachHang?> TimTheoSoGiayTo(string soGiayTo)
-    {
-        const string sql = "SELECT * FROM KhachHang WHERE SoGiayTo=@SoGiayTo";
-        return await PhienDuLieu.Session.Connection.QuerySingleOrDefaultAsync<KhachHang>(sql,
-            new { SoGiayTo = soGiayTo }, PhienDuLieu.Session.Transaction);
-    }
-
-    public static async Task Them(KhachHang khachHang)
-    {
-        const string sql = """
-            INSERT INTO KhachHang (MaKH,HoTen,NgaySinh,GioiTinh,QuocTich,LoaiGiayTo,SoGiayTo,DiaChiThuongTru,SDT,Email)
-            VALUES (@MaKH,@HoTen,@NgaySinh,@GioiTinh,@QuocTich,@LoaiGiayTo,@SoGiayTo,@DiaChiThuongTru,@SDT,@Email)
-            """;
-        if (await PhienDuLieu.Session.Connection.ExecuteAsync(sql, khachHang, PhienDuLieu.Session.Transaction) != 1)
-            throw new InvalidOperationException("Không thể thêm khách hàng.");
-    }
 }
